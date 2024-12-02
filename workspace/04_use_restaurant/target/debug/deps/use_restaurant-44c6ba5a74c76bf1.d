e8eb6770f0d7b8973c66e53d568f54efb143d57e
Kauan Perdigão <kauanopl@alu.ufc.br>
2024-12-02 16:33:55 -0300

    first commit

---
+++ workspace/04_use_restaurant/target/debug/deps/use_restaurant-44c6ba5a74c76bf1.d
@@ -0,0 +1,5 @@
+C:\Users\kauan\OneDrive\Documentos\Reuso de Software\ufc_2024_2\RS\projetos\aula\04_use_restaurant\target\debug\deps\libuse_restaurant-44c6ba5a74c76bf1.rmeta: src/main.rs
+
+C:\Users\kauan\OneDrive\Documentos\Reuso de Software\ufc_2024_2\RS\projetos\aula\04_use_restaurant\target\debug\deps\use_restaurant-44c6ba5a74c76bf1.d: src/main.rs
+
+src/main.rs:
